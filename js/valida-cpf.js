ddd6b2e80437e2bd825721c9a0b5f71870076f76
Leticia1508 <leticiademoraes0@gmail.com>
2024-02-28 23:46:08 -0300

    feat: atualizado formuario

---
+++ js/valida-cpf.js
@@ -0,0 +1,32 @@
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    validaNumerosRepetidos(cpf);
+
+    console.log(validaNumerosRepetidos(cpf));
+}
+
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return numerosRepetidos.includes(cpf)
+}
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+     for (let tamanho =0; tamanho < 9; tamanho++) {
+        soma += cpf [tamanho] * multiplicado;
+        multiplicador--
+     }
+
+}
